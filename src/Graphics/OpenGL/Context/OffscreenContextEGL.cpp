d3aa4c1b40fd7bbdbac21ad3d3a99eee52b634fd
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-02-19 09:49:00 +0100

    Added fallback EGL headers.

--- src/Graphics/OpenGL/Context/OffscreenContextEGL.cpp
+++ src/Graphics/OpenGL/Context/OffscreenContextEGL.cpp
@@ -29,8 +29,13 @@
 #include <cstring>
 
 #define EGL_EGLEXT_PROTOTYPES
+#if __has_include(<EGL/egl.h>)
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+#else
+#include "../../Vulkan/libs/EGL/egl.h"
+#include "../../Vulkan/libs/EGL/eglext.h"
+#endif
 
 #include <Utils/StringUtils.hpp>
 #include <Utils/File/Logfile.hpp>
