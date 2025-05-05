4d6ef4ab38d58147cde9fbc102b0ff8c63178bdf
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2025-05-05 22:02:25 +0200

    Added support for builds without glm of Vulkan renderer (4).

--- src/Utils/AppSettings.hpp
+++ src/Utils/AppSettings.hpp
@@ -31,7 +31,10 @@
 
 #include <map>
 #include <string>
+#ifdef USE_GLM
 #include <glm/vec2.hpp>
+#endif
+
 #include <Defs.hpp>
 #include <Utils/Singleton.hpp>
 #include <Utils/Convert.hpp>
@@ -241,8 +244,10 @@ public:
     int getNumDisplays();
     void getCurrentDisplayMode(int& width, int& height, int& refreshRate, int displayIndex = 0);
     void getDesktopDisplayMode(int& width, int& height, int& refreshRate, int displayIndex = 0);
+#ifdef USE_GLM
     glm::ivec2 getCurrentDisplayModeResolution(int displayIndex = 0);
     glm::ivec2 getDesktopResolution(int displayIndex = 0);
+#endif
     void captureMouse(bool _capture);
 
     // Get the directory where the application data is stored.
