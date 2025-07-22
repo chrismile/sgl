dd3e2209a1f78f84c0d745ab89dc77fb645fcd36
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2025-07-22 19:31:57 +0200

    Added DirectX headers submodule.

--- src/Graphics/D3D12/Render/Renderer.cpp
+++ src/Graphics/D3D12/Render/Renderer.cpp
@@ -63,20 +63,18 @@ Renderer::Renderer(Device* device, uint32_t numDescriptors) : device(device) {
     ThrowIfFailed(d3d12Device->CreateCommandList(
             0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&commandList)));
 
-    float clearColor[] = { 0.4f, 0.6f, 0.9f, 1.0f };
+    // STDCALL vs. THISCALL on MSVC and other compilers: https://stackoverflow.com/a/34322863
+    // More info: https://github.com/dotnet/coreclr/pull/23974
+#if defined(_MSC_VER) || !defined(_WIN32)
     auto baseRtv = descriptorHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_RTV]->GetCPUDescriptorHandleForHeapStart();
-    D3D12_CPU_DESCRIPTOR_HANDLE rtv;
-    UINT rtvDescIncrementSize = d3d12Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-    rtv.ptr = baseRtv.ptr + 0 * rtvDescIncrementSize;
-
-    D3D12_RESOURCE_BARRIER resourceBarrier{};
-    resourceBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-    resourceBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
-    resourceBarrier.Transition.pResource = nullptr;
-    resourceBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COMMON;
-    resourceBarrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
-    commandList->ResourceBarrier(1, &resourceBarrier);
-    commandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
+#else
+    D3D12_CPU_DESCRIPTOR_HANDLE baseRtv;
+    descriptorHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_RTV]->GetCPUDescriptorHandleForHeapStart(&baseRtv);
+#endif
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtv(baseRtv);
+    //D3D12_CPU_DESCRIPTOR_HANDLE rtv;
+    //UINT rtvDescIncrementSize = d3d12Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    //rtv.ptr = baseRtv.ptr + 0 * rtvDescIncrementSize;
 
     commandList->Close();
     ID3D12CommandList* const commandLists[] = {
@@ -106,7 +104,7 @@ Renderer::Renderer(Device* device, uint32_t numDescriptors) : device(device) {
     size_t bufferSize = 1024;
     auto* bufferData = new float[bufferSize];
 
-    D3D12_HEAP_PROPERTIES heapProperties{};
+    /*D3D12_HEAP_PROPERTIES heapProperties{};
     heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
     D3D12_RESOURCE_DESC bufferResourceDesc{};
     bufferResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
@@ -125,17 +123,77 @@ Renderer::Renderer(Device* device, uint32_t numDescriptors) : device(device) {
             &bufferResourceDesc,
             D3D12_RESOURCE_STATE_GENERIC_READ,
             nullptr,
+            IID_PPV_ARGS(&intermediateResource)));*/
+    D3D12_RESOURCE_FLAGS flags{};
+    CD3DX12_HEAP_PROPERTIES heapPropertiesDefault(D3D12_HEAP_TYPE_DEFAULT);
+    auto bufferResourceDesc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize, flags);
+    ThrowIfFailed(d3d12Device->CreateCommittedResource(
+            &heapPropertiesDefault,
+            D3D12_HEAP_FLAG_NONE,
+            &bufferResourceDesc,
+            D3D12_RESOURCE_STATE_COMMON,
+            nullptr,
+            IID_PPV_ARGS(&destinationResource)));
+    CD3DX12_HEAP_PROPERTIES heapPropertiesUpload(D3D12_HEAP_TYPE_DEFAULT);
+    auto bufferDescUpload = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
+    ThrowIfFailed(d3d12Device->CreateCommittedResource(
+            &heapPropertiesUpload,
+            D3D12_HEAP_FLAG_NONE,
+            &bufferDescUpload,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
             IID_PPV_ARGS(&intermediateResource)));
 
+    ComPtr<ID3D12Resource> depthImageResource{};
+    D3D12_CLEAR_VALUE optimizedClearValue = {};
+    optimizedClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+    optimizedClearValue.DepthStencil = { 1.0f, 0 };
+    auto depthTexResource = CD3DX12_RESOURCE_DESC::Tex2D(
+            DXGI_FORMAT_D32_FLOAT, 1024, 1024,
+            1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL);
+    ThrowIfFailed(d3d12Device->CreateCommittedResource(
+            &heapPropertiesDefault, D3D12_HEAP_FLAG_NONE,
+            &depthTexResource,
+            D3D12_RESOURCE_STATE_DEPTH_WRITE,
+            &optimizedClearValue,
+            IID_PPV_ARGS(&depthImageResource)
+    ));
+
+    ComPtr<ID3D12Resource> colorImageResource{};
+    float clearColor[] = { 0.4f, 0.6f, 0.9f, 1.0f };
+    optimizedClearValue.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+    memcpy(optimizedClearValue.Color, clearColor, sizeof(float) * 4);
+    auto texResource = CD3DX12_RESOURCE_DESC::Tex2D(
+            DXGI_FORMAT_R32G32B32A32_FLOAT, 1024, 1024,
+            1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL);
+    ThrowIfFailed(d3d12Device->CreateCommittedResource(
+            &heapPropertiesDefault, D3D12_HEAP_FLAG_NONE,
+            &texResource,
+            D3D12_RESOURCE_STATE_DEPTH_WRITE,
+            &optimizedClearValue,
+            IID_PPV_ARGS(&colorImageResource)
+    ));
+
+    /*D3D12_RESOURCE_BARRIER resourceBarrier{};
+    resourceBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    resourceBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+    resourceBarrier.Transition.pResource = nullptr;
+    resourceBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COMMON;
+    resourceBarrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;*/
+    CD3DX12_RESOURCE_BARRIER resourceBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
+            colorImageResource.Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+    commandList->ResourceBarrier(1, &resourceBarrier);
+    commandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
+
     // TODO: https://github.com/microsoft/DirectX-Headers/blob/main/include/directx/d3dx12_resource_helpers.h
-    /*D3D12_SUBRESOURCE_DATA subresourceData = {};
+    D3D12_SUBRESOURCE_DATA subresourceData = {};
     subresourceData.pData = bufferData;
     subresourceData.RowPitch = LONG_PTR(bufferSize);
     subresourceData.SlicePitch = subresourceData.RowPitch;
     UpdateSubresources(
             commandList.Get(),
             destinationResource.Get(), intermediateResource.Get(),
-            0, 0, 1, &subresourceData);*/
+            0, 0, 1, &subresourceData);
     delete[] bufferData;
 
     uint64_t resourceIdx = 0;
--- src/Graphics/D3D12/Utils/d3d12.hpp
+++ src/Graphics/D3D12/Utils/d3d12.hpp
@@ -46,10 +46,16 @@
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
-#include <d3d12.h>
+#include <directx/d3d12.h>
 #include <dxgi1_6.h>
+#include <directx/d3dx12.h>
+#ifndef _MSC_VER
+#include <dxguids/dxguids.h>
+#endif
+
+#ifdef SUPPORT_D3D_COMPILER
 #include <d3dcompiler.h>
-//#include <d3dx12.h>
+#endif
 
 inline void ThrowIfFailed(HRESULT hr) {
     if (FAILED(hr)) {
