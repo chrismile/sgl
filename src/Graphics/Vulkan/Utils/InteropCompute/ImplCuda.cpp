9a990784d4bdfbd68e3865ecf9eaf57115b7e2ad
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2025-08-06 23:20:33 +0200

    Added initial virtual external semaphore class.

---
+++ src/Graphics/Vulkan/Utils/InteropCompute/ImplCuda.cpp
@@ -0,0 +1,85 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2025, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ImplCuda.hpp"
+
+namespace sgl { namespace vk {
+
+#ifdef _WIN32
+void SemaphoreVkCudaInterop::setExternalSemaphoreWin32Handle(HANDLE handle) {
+    // https://github.com/intel/llvm/blob/sycl/sycl/doc/extensions/experimental/sycl_ext_oneapi_bindless_images.asciidoc
+    sycl::ext::oneapi::experimental::external_semaphore_handle_type semaphoreHandleType;
+    if (isTimelineSemaphore()) {
+#ifndef SYCL_NO_EXTERNAL_TIMELINE_SEMAPHORE_SUPPORT
+        semaphoreHandleType = sycl::ext::oneapi::experimental::external_semaphore_handle_type::timeline_win32_nt_handle;
+#else
+        sgl::Logfile::get()->throwError(
+            "Error in SemaphoreVkComputeApiInterop::SemaphoreVkComputeApiInterop: "
+            "The installed version of SYCL does not support external timeline semaphores.");
+#endif
+    } else {
+        semaphoreHandleType = sycl::ext::oneapi::experimental::external_semaphore_handle_type::win32_nt_handle;
+    }
+    sycl::ext::oneapi::experimental::external_semaphore_descriptor<sycl::ext::oneapi::experimental::resource_win32_handle>
+        syclExternalSemaphoreDescriptor{handle, semaphoreHandleType};
+    auto* wrapper = new SyclExternalSemaphoreWrapper;
+    wrapper->syclExternalSemaphore = sycl::ext::oneapi::experimental::import_external_semaphore(
+        syclExternalSemaphoreDescriptor, *g_syclQueue);
+    externalSemaphore = reinterpret_cast<void*>(wrapper);
+}
+#endif
+
+#ifdef __linux__
+void SemaphoreVkCudaInterop::setExternalSemaphoreFd(int fd) {
+    // https://github.com/intel/llvm/blob/sycl/sycl/doc/extensions/experimental/sycl_ext_oneapi_bindless_images.asciidoc
+    sycl::ext::oneapi::experimental::external_semaphore_handle_type semaphoreHandleType;
+    if (isTimelineSemaphore()) {
+#ifndef SYCL_NO_EXTERNAL_TIMELINE_SEMAPHORE_SUPPORT
+        semaphoreHandleType = sycl::ext::oneapi::experimental::external_semaphore_handle_type::timeline_fd;
+#else
+        sgl::Logfile::get()->throwError(
+            "Error in SemaphoreVkComputeApiInterop::SemaphoreVkComputeApiInterop: "
+            "The installed version of SYCL does not support external timeline semaphores.");
+#endif
+    } else {
+        semaphoreHandleType = sycl::ext::oneapi::experimental::external_semaphore_handle_type::opaque_fd;
+    }
+    sycl::ext::oneapi::experimental::external_semaphore_descriptor<sycl::ext::oneapi::experimental::resource_fd>
+        syclExternalSemaphoreDescriptor{fileDescriptor, semaphoreHandleType};
+    auto* wrapper = new SyclExternalSemaphoreWrapper;
+    wrapper->syclExternalSemaphore = sycl::ext::oneapi::experimental::import_external_semaphore(
+        syclExternalSemaphoreDescriptor, *g_syclQueue);
+    externalSemaphore = reinterpret_cast<void*>(wrapper);
+}
+#endif
+
+SemaphoreVkCudaInterop::~SemaphoreVkCudaInterop() {
+    sycl::ext::oneapi::experimental::release_external_semaphore(syclExternalSemaphore, *g_syclQueue);
+}
+
+}}
---
+++ src/Graphics/Vulkan/Utils/InteropCompute/ImplCuda.hpp
@@ -0,0 +1,62 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2025, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef SGL_IMPLCUDA_HPP
+#define SGL_IMPLCUDA_HPP
+
+#include "Common.hpp"
+
+namespace sgl { namespace vk {
+
+#ifdef SUPPORT_SYCL_INTEROP
+// For more information on SYCL interop:
+// https://github.com/intel/llvm/blob/sycl/sycl/doc/extensions/experimental/sycl_ext_oneapi_bindless_images.asciidoc
+DLL_OBJECT void setGlobalSyclQueue(sycl::queue& syclQueue);
+#endif
+
+class SemaphoreVkCudaInterop : public SemaphoreVkComputeApiInterop {
+public:
+    ~SemaphoreVkCudaInterop() override;
+
+protected:
+#ifdef _WIN32
+    void setExternalSemaphoreWin32Handle(HANDLE handle) override;
+#endif
+#ifdef __linux__
+    void setExternalSemaphoreFd(int fd) override;
+#endif
+    void importExternalSemaphore() override;
+
+private:
+    void* externalSemaphore;
+};
+
+}}
+
+#endif //SGL_IMPLCUDA_HPP
