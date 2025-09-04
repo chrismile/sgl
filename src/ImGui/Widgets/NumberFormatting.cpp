b0ae84edfd51d1e959297deac5a1c308dedb6a24
Christoph Neuhauser <christoph.neuhauser@intel.com>
2025-09-04 13:03:20 +0200

    Added missing include of limits header for recent GCC versions.

--- src/ImGui/Widgets/NumberFormatting.cpp
+++ src/ImGui/Widgets/NumberFormatting.cpp
@@ -26,6 +26,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <limits>
 #include <algorithm>
 #include <Utils/Convert.hpp>
 #include "NumberFormatting.hpp"
